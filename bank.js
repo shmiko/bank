eae74f4ca98b2da89704232dd3c7ce1f5e01fc17
Paul Jones <shmiko@gmail.com>
2016-02-05 21:52:46 +1100

    final

--- bank.js
+++ bank.js
@@ -96,4 +96,4 @@ var exitingJensAccount = getAccount('jen001');
 console.log("Checking for Jens account ",exitingJensAccount);
 
 //ok done for now, will need to add user auth and sso
-// and karma tests and wrap a gui for visuals and css for styles and gulp
+// and karma tests and wrap a gui for visuals and css for styles and gulp.
