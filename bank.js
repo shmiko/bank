fde3fccdcb5dd10d02e19ac28a5e01056b88fac8
Paul Jones <shmiko@gmail.com>
2016-12-17 10:51:22 +1100

    add balance validation

--- bank.js
+++ bank.js
@@ -2,6 +2,8 @@ var account = {
 	balance : 0
 }
 
+var isInDebt = false;
+
 function deposit(account, amount){
 	console.log("the deposit amount is ", amount);
 	var newAmount = account.balance + amount;
@@ -22,10 +24,17 @@ function withdraw(account, amount){
 
 function getBalance(account){
 	console.log("the current balance is ", account.balance);
+	if (account.balance < 0){
+		isInDebt = true;
+	}
+	console.log("Your balance is in the negative, you owe $", account.balance);
 	return account.balance;
 }
 
 //tests
 deposit(account,12);
 withdraw(account,6);
+getBalance(account);
+
+withdraw(account,26);
 getBalance(account);
