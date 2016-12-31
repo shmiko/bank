6fed4df8d501d673760953c5173d4357bba6055a
Paul Jones <shmiko@gmail.com>
2016-12-31 21:45:36 +1100

    final

--- bank.js
+++ bank.js
@@ -95,4 +95,4 @@ console.log("accounts should be ",accounts);
 var exitingJensAccount = getAccount('jen001');
 console.log("Checking for Jens account ",exitingJensAccount);
 
-//ok done for now
+//ok done for now, will need to add user
