5e59f99d582ce39d8429a9ccdbb4113fdd9419a9
Paul Jones <shmiko@gmail.com>
2017-01-10 08:42:22 +1100

    fixes typescript

--- bank.js
+++ bank.js
@@ -39,8 +39,6 @@ function deposit(account, amount){
 	return newAmount; //not required
 }
 
-
-//lll
 function withdraw(account, amount){
 	console.log("the withdraw amount is ", amount);
 	var newAmount = account.balance - amount;
